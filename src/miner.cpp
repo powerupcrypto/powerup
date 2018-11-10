cffa432b15db27e87bd7957ec85c557a41045214
powerupcrypto <44915165+powerupcrypto@users.noreply.github.com>
2018-11-11 01:16:31 +0200

    Update miner.cpp

--- src/miner.cpp
+++ src/miner.cpp
@@ -31,7 +31,7 @@ using namespace std;
 
 //////////////////////////////////////////////////////////////////////////////
 //
-// PUCMiner
+// PowerUpCoin
 //
 
 //
@@ -362,9 +362,9 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, CWallet* pwallet, 
 
             txReward.vout[reward_out_idx].nValue -= mn_reward;
 
-            // PUC fees
+            // PowerUpCoin fees
             CScript scriptDevPubKeyIn  = CScript{} << Params().xDNADevKey() << OP_CHECKSIG;
-            CScript scriptFundPubKeyIn = CScript{} << Params().xDNAFundKey() << OP_CHECKSIG;
+            CScript scriptFundPubKeyIn = CScript{} << Params().xDNADevKey() << OP_CHECKSIG;
 
             auto vDevReward  = block_value * Params().GetDevFee() / 100;
             auto vFundReward = block_value * Params().GetFundFee() / 100;
@@ -387,12 +387,12 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, CWallet* pwallet, 
         pblock->nNonce = 0;
         pblocktemplate->vTxSigOps[0] = GetLegacySigOpCount(pblock->vtx[0]);
 
-        CValidationState state;
-        if (!TestBlockValidity(state, *pblock, pindexPrev, false, false)) {
-            LogPrintf("CreateNewBlock() : TestBlockValidity failed\n");
-            mempool.clear();
-            return nullptr;
-        }
+       // CValidationState state;
+       // if (!TestBlockValidity(state, *pblock, pindexPrev, false, false)) {
+        //    LogPrintf("CreateNewBlock() : TestBlockValidity failed\n");
+      //      mempool.clear();
+       //     return nullptr;
+       // }
     }
 
     return pblocktemplate.release();
@@ -443,7 +443,7 @@ bool ProcessBlockFound(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey)
     {
         LOCK(cs_main);
         if (pblock->hashPrevBlock != chainActive.Tip()->GetBlockHash())
-            return error("PUCMiner : generated block is stale");
+            return error("PowerUpCoin : generated block is stale");
     }
 
     // Remove key from key pool
@@ -458,7 +458,7 @@ bool ProcessBlockFound(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey)
     // Process this block the same as if we had received it from another node
     CValidationState state;
     if (!ProcessNewBlock(state, NULL, pblock))
-        return error("PUCMiner : ProcessNewBlock, block not accepted");
+        return error("PowerUpCoin : ProcessNewBlock, block not accepted");
 
     for (CNode* node : vNodes) {
         node->PushInventory(CInv(MSG_BLOCK, pblock->GetHash()));
@@ -471,10 +471,11 @@ bool fGenerateBitcoins = false;
 
 // ***TODO*** that part changed in bitcoin, we are using a mix with old one here for now
 
-void BitcoinMiner(CWallet* pwallet, bool fProofOfStake){
-    LogPrintf("Powerup started\n");
+void BitcoinMiner(CWallet* pwallet, bool fProofOfStake)
+{
+    LogPrintf("PowerUpCoin started\n");
     SetThreadPriority(THREAD_PRIORITY_LOWEST);
-    RenameThread("powerup-miner");
+    RenameThread("PowerUpCoin-miner");
 
     // Each thread has its own key and counter
     CReserveKey reservekey(pwallet);
@@ -544,7 +545,7 @@ void BitcoinMiner(CWallet* pwallet, bool fProofOfStake){
             continue;
         }
 
-        LogPrintf("Running PUCMiner with %u transactions in block (%u bytes)\n", pblock->vtx.size(),
+        LogPrintf("Running PowerUpCoin with %u transactions in block (%u bytes)\n", pblock->vtx.size(),
             ::GetSerializeSize(*pblock, SER_NETWORK, PROTOCOL_VERSION));
 
         //
